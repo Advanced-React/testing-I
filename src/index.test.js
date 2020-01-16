fe8bcb5cbf2c96a17a2c8a903fe3445a71df16b9
Justin Trombley <jtrombley96@gmail.com>
2020-01-16 18:47:48 -0500

    Excersize 1

--- src/index.test.js
+++ src/index.test.js
@@ -1,14 +1,20 @@
-import {sum} from './index.js'
+import { sum } from './index.js';
+// test('sum returns the sum of two numbers', () => {
+// AAA
+// Arrange- let's us set up any configuration we need for this test
+// let first = 2;
+// let second = 2;
+// let expected = 4;
+
+// ACT
+// let actual = sum(first, second);
+
+// ASSERT
+// expect(actual).toBe(expected);
+// });
+
 test('sum returns the sum of two numbers', () => {
-	// AAA
-	// Arrange- let's us set up any configuration we need for this test
-	let first = 2;
-	let second = 2;
-	let expected = 4;
-
-	// ACT
-	let actual = sum(first, second);
-
-	// ASSERT
-	expect(actual).toBe(expected);
+	//      Act  Arrange     Assert
+	expect(sum(2, 2)).toBe(4);
+	expect(sum(2, 3)).toBe(5);
 });
